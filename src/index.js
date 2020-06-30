19f3d8e254d11ee89cf8df761f37eb8871f1e3a6
petrero <petrero@gmail.com>
2020-06-30 09:28:08 +0300

    19.2.Referencing JS Variables in JSX(Section 2: Building Content with JSX)
    We're not only limited to making very simple statements like this where we define a single variable
    
    at a time and then reference it inside.
    
    We could very easily do something a little bit more complicated for example maybe in front or right
    
    before my app component.
    
    I defined something like say function get button text and then anytime someone calls this function maybe
    
    I will return click on me.
    
    Then inside of my button tag right here.
    
    Rather than referring to button text I can delete that and call the function that we just created.
    
    So I'll say it button text like so make sure you get the parentheses on there so you actually invoke
    
    the function.
    
    The other thing it wants to do is clean up that variable that we're no longer using but in text Revier.
    
    All right let's save this and see how it goes.
    
    I'll flip back over and of course see now see.
    
    It's the button says Click on me as opposed to the Submit or click me that it said before.
    
    Now there's only a couple limitations around what type of variable we can reference inside of these
    
    GSX blocks using these curly braces.
    
    Let me give you a couple of quick examples.
    
    In the next video because this is a problem that you are very likely to run into at some point during
    
    your rehab act career.
    
    So quick pause and I'll see you in just a minute.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 //Import the react and ReactDOM library
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function getButtonText(){
+  return 'Click Me!';
+}
+
 //Create a react component
 const App = () => {
-  const buttonText = 'Click Me!';
-
   return (
     <div>
       <label className="label" htmlFor="name">Enter name:</label>
       <input id="name" type="text"/>
       <button style={{backgroundColor: 'blue', color: 'white'}}>
-        {buttonText}
+        {getButtonText()}
       </button>
     </div>
   )
